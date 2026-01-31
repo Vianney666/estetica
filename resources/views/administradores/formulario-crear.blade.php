b7dde9b19a375865fc85003cfd9e62ff13ba3a31
viann <2124100011@soy.utj.edu.mx>
2026-01-30 18:26:07 -0600

    ya quedo

--- resources/views/administradores/formulario-crear.blade.php
+++ resources/views/administradores/formulario-crear.blade.php
@@ -125,7 +125,7 @@
         </div>
         <button type="submit"
             class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-purple-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-            Add product
+            Guardar
         </button>
         </form>
         </div>
